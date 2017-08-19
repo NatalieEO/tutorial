e0a75beb18fc1b5642f6f9b6bcf13535164d3462
Natalie Perez <NataliePerezEO@gmail.com>
2017-08-19 00:43:58 +0000

    changed default email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'NataliePEO@gmail.com'
+   default to: 'NataliePerezEO@gmail.com'
    
    def contact_email(name, email, body)
       @name = name
